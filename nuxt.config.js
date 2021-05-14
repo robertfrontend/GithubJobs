f3db386a32849191fc4c3613627f3b0b5b9d8c67
Robert Romero Medina <enanow41romero@gmail.com>
2021-05-14 08:25:16 -0400

    new change

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
+  target: 'static',
+
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
